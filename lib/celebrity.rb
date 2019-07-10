ef7608ac81ce3537d9468af0a7f127fbad9ec4a7
ghatzid <ghatzid@gmail.com>
2019-07-10 13:51:37 -0400

    added seed method to celebrity.rb

--- lib/celebrity.rb
+++ lib/celebrity.rb
@@ -1,4 +1,8 @@
 class Celebrity < ActiveRecord::Base
   has_many :celebrity_interests
   has_many :interests, through: :celebrity_interests
+
+  def self.seed_interests
+    Celebrity.all.map {|celeb| celeb.interest_ids = rand(1..499)}
+  end
 end
