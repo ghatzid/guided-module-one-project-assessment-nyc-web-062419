c715b45e2b5cd1a4767f0ae8531c055df612b435
ghatzid <ghatzid@gmail.com>
2019-07-11 13:29:44 -0400

    unsure

--- lib/user.rb
+++ lib/user.rb
@@ -37,10 +37,81 @@ class User < ActiveRecord::Base
     main_menu
   end
   
+  def User.main_menu
+    puts "Please choose from the following options:"
+    puts "1. View your profile"
+    puts "2. Add interests to your profile" #calls interest_prompt
+    puts "3. View your matches" ### calls matchmaker
+    puts "4. Delete your profile"
+    puts "5. Exit"
+    inp = gets.chomp
+    # binding.pry
+    case inp
+    when "1" 
+      view_user_profile
+    when "2"
+      interest_prompt #Bring over user prompt to user class
+    when "3"
+      matchmaker #Get matchmaker running here
+    when "4"
+      delete_user
+    when "5"
+      exit
+    else
+      invalid_response("a valid number from the list")
+      main_menu
+    end
+  end
+  
+
+
+  def interest_prompt
+    array = []
+    5.times do
+      array.push(Interest.all.sample)
+    end  
+    puts "Type a number to chose an Interest"    
+    puts "1. #{array[0].interest_names}"
+    puts "2. #{array[1].interest_names}"
+    puts "3. #{array[2].interest_names}"
+    puts "4. #{array[3].interest_names}"
+    puts "5. #{array[4].interest_names}"
+    puts 'hit "m" for more choices'
+    inp = gets.chomp
+    case inp
+      when "1" 
+        self.interest_ids = array[0].id
+        user_options
+      when "2"
+        self.interest_ids = array[1].id
+        user_options
+      when "3"
+        self.interest_ids = array[2].id
+        user_options
+      when "4"
+        self.interest_ids = array[3].id
+        user_options
+      when "5"
+        self.interest_ids = array[4].id
+        user_options
+      else
+        puts "#{inp} is not a valid answer"
+        invalid_response(a valid number from the list)
+        interest_prompt
+    end
+  end 
+
+
   def self.find_user
     puts "What is your name?"
     inp = gets.chomp
-    @user = User.all.find {|x| x.name == inp}
+      if User.all.find {|x| x.name == inp}
+        @user = User.all.find {|x| x.name == inp}
+      else
+        puts "Sorry our system...."
+        sleep 1
+        name_prompt
+      end
     #what if no user is found?
   end
   
