cbcae921e0b5ae314569903e030435974d8872d2
ghatzid <ghatzid@gmail.com>
2019-07-11 16:42:21 -0400

    done

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -12,7 +12,7 @@ class CommandLineInterface
     puts "888    888      888      888  Y8P  888    d88P   888  888    888 ".light_red
     puts "888  .d88P      888      888       888   d8888888888  Y88b  d88P ".light_red
     puts "8888888P'       888      888       888  d88P     888   'Y8888P'  ".light_red
-    puts "\n"
+    puts "\t(Do \tYou \tMatch \tA \tCelebrity?)\n".light_red 
   end
   
   def initial_question
@@ -72,13 +72,13 @@ class CommandLineInterface
       @user = User.all.find {|x| x.name == inp}
       @user.main_menu
     else
-      puts "Sorry #{inp} not found"
+      puts "\nSorry #{inp} not found"
       user_not_found
     end
   end
   
   def user_not_found
-    puts "Please choose an option"
+    puts "Please choose an option:"
     puts "1. Search again"
     puts "2. Create new user"
     var = gets.chomp
